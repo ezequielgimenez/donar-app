cb5f7b0fce56bd78090ab859fd7cee29f4202031
Ezequiel <ezequielezequiel9@gmail.com>
2025-06-06 17:50:24 -0300

    edit change connections

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT!);
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
